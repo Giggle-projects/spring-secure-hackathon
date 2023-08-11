4bd46dffe18255c4d78aa923cebc7b6fa2ddd8df
ecsimsw <ecsimsw@gmail.com>
2023-08-12 01:18:15 +0900

    feat : add ApplicationType

---
+++ src/main/java/se/ton/t210/domain/ApplicationType.java
@@ -0,0 +1,10 @@
+package se.ton.t210.domain;
+
+public enum ApplicationType {
+    PoliceOfficerMale,
+    PoliceOfficerFemale,
+    FireOfficerMale,
+    FireOfficerFemale,
+    CorrectionalOfficerMale,
+    CorrectionalOfficerFemale
+}
