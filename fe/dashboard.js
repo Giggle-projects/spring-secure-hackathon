dad5a24a4151e5ef5bdaf40238ca8822d71950c2
ecsimsw <ecsimsw@gmail.com>
2023-08-13 05:41:03 +0900

    feat : add frontend

---
+++ fe/dashboard.js
@@ -0,0 +1,19 @@
+const settingContainer = document.getElementById("settingContainer");
+settingContainer.addEventListener("click", function (e) {
+    window.location.href = "./setting-account.html";
+});
+
+const menu02Container = document.getElementById("menu02Container");
+menu02Container.addEventListener("click", function (e) {
+    window.location.href = "./record.html";
+});
+
+const menu03Container = document.getElementById("menu03Container");
+menu03Container.addEventListener("click", function (e) {
+    window.location.href = "./personal-information.html";
+});
+
+const menu04Container = document.getElementById("menu04Container")
+menu04Container.addEventListener("click", function (e) {
+    window.location.href = "./application-information1.html";
+});
