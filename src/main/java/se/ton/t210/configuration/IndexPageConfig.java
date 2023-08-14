3b3fbcbacc9adab742a3bf352d6f2056e5d9d919
ecsimsw <ecsimsw@gmail.com>
2023-08-15 04:47:16 +0900

    feat : index page

---
+++ src/main/java/se/ton/t210/configuration/IndexPageConfig.java
@@ -0,0 +1,15 @@
+package se.ton.t210.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class IndexPageConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+            .setViewName("redirect:./html/sign-in.html");
+    }
+}
