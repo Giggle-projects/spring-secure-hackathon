50bddf54ad8ef646be73f502ead6d7f0420d9989
ecsimsw <ecsimsw@gmail.com>
2023-08-15 20:00:15 +0900

    feat : remove unused params

--- src/main/java/se/ton/t210/dto/ScoreResponse.java
+++ src/main/java/se/ton/t210/dto/ScoreResponse.java
@@ -6,14 +6,8 @@ import lombok.Getter;
 public class ScoreResponse {
 
     private final int score;
-    private final int maxScore;
-
-    public ScoreResponse(int score, int maxScore) {
-        this.score = score;
-        this.maxScore = maxScore;
-    }
 
     public ScoreResponse(int score) {
-        this(score, 0);
+        this.score = score;
     }
 }
