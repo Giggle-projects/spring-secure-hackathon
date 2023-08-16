ab67160fd6f94ee7bd7172a4ece6c8160318470e
good0619@naver.com <32181771@dankook.ac.kr>
2023-08-16 20:01:24 +0900

    feat : ml training dummy data

--- src/main/java/se/ton/t210/utils/data/initData/MLTrainingCsvLine.java
+++ src/main/java/se/ton/t210/utils/data/initData/MLTrainingCsvLine.java
@@ -5,21 +5,35 @@ import se.ton.t210.domain.type.ApplicationType;
 import java.util.Random;
 
 // ML
-// id,applicationType,score,month
+// id,member_id,applicationType,score,month
 public class MLTrainingCsvLine implements CsvLineStrategy {
 
     private static final Random RANDOM = new Random();
     private Long id = 0L;
+    private Long userId = 0L;
+    private int month = 0;
+    private String applicationTypeName;
 
     @Override
     public String retrieveNewLine() {
-        id++;
+        setColumnElement();
         return String.join(",",
                 String.valueOf(id),
-                randomApplicationTypeStandardName(),
+                String.valueOf(userId / 12),
+                applicationTypeName,
                 randomNum(5, 50),
-                randomNum(1, 12)
+                String.valueOf(month)
         );
+    }
+
+    private void setColumnElement() {
+        id++;
+        userId++;
+        month = month % 12;
+        if (month == 0) {
+            applicationTypeName = randomApplicationTypeStandardName();
+        }
+        month++;
     }
 
     private String randomNum(int min, int max) {
