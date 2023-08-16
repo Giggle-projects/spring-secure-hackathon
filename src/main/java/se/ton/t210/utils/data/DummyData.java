443a57cfd4d1bc0ebaaa138d854a69af44786246
good0619@naver.com <32181771@dankook.ac.kr>
2023-08-16 19:12:18 +0900

    refactor : create dummy data

--- src/main/java/se/ton/t210/utils/data/DummyData.java
+++ src/main/java/se/ton/t210/utils/data/DummyData.java
@@ -70,7 +70,7 @@ class DummyData {
         memberRepository.saveAll(members);
     }
 
-    private Map<ApplicationType, List<EvaluationItem>> createEvaluationItemTable() {
+    private void createEvaluationItemTable() {
         itemTable = new HashMap<>();
         itemTable.put(ApplicationType.PoliceOfficerMale, createPoliceOfficerMaleEvaluationItem(ApplicationType.PoliceOfficerMale));
         itemTable.put(ApplicationType.PoliceOfficerFemale, createPoliceOfficerFeMaleEvaluationItem(ApplicationType.PoliceOfficerFemale));
@@ -78,22 +78,19 @@ class DummyData {
         itemTable.put(ApplicationType.FireOfficerFemale, createFireOfficerFeMaleEvaluationItem(ApplicationType.FireOfficerFemale));
         itemTable.put(ApplicationType.CorrectionalOfficerMale, createCorrectionalOfficerMaleEvaluationItem(ApplicationType.CorrectionalOfficerMale));
         itemTable.put(ApplicationType.CorrectionalOfficerFemale, createCorrectionalOfficerFeMaleEvaluationItem(ApplicationType.CorrectionalOfficerFemale));
-        return itemTable;
     }
 
     @Transactional
     public List<EvaluationItem> createPoliceOfficerMaleEvaluationItem(ApplicationType applicationType) {
         final List<EvaluationItem> policeOfficerMaleData = getPoliceOfficeItemName(applicationType);
-
         evaluationItemRepository.saveAll(policeOfficerMaleData);
-
-        final List<List<Float>> policeOfficerMaleSectionData = new ArrayList<>();
-        policeOfficerMaleSectionData.add(List.of(0F, 40F, 43F, 46F, 49F, 52F, 55F, 58F, 61F, 64F));
-        policeOfficerMaleSectionData.add(List.of(0F, 16F, 22F, 28F, 34F, 40F, 46F, 51F, 56F, 61F));
-        policeOfficerMaleSectionData.add(List.of(0F, 32F, 36F, 40F, 43F, 46F, 49F, 52F, 55F, 58F));
-        policeOfficerMaleSectionData.add(List.of(0F, 8.68F, 8.46F, 8.26F, 8.05F, 7.84F, 7.63F, 7.42F, 7.21F, 7F));
-        policeOfficerMaleSectionData.add(List.of(0F, 35F, 41F, 47F, 52F, 57F, 62F, 67F, 72F, 77F));
-
+        final List<List<Float>> policeOfficerMaleSectionData = List.of(
+                List.of(0F, 40F, 43F, 46F, 49F, 52F, 55F, 58F, 61F, 64F),
+                List.of(0F, 16F, 22F, 28F, 34F, 40F, 46F, 51F, 56F, 61F),
+                List.of(0F, 32F, 36F, 40F, 43F, 46F, 49F, 52F, 55F, 58F),
+                List.of(0F, 8.68F, 8.46F, 8.26F, 8.05F, 7.84F, 7.63F, 7.42F, 7.21F, 7F),
+                List.of(0F, 35F, 41F, 47F, 52F, 57F, 62F, 67F, 72F, 77F)
+        );
         saveEvaluationSectionScore(policeOfficerMaleData, policeOfficerMaleSectionData);
         return policeOfficerMaleData;
     }
@@ -101,17 +98,14 @@ class DummyData {
     @Transactional
     public List<EvaluationItem> createPoliceOfficerFeMaleEvaluationItem(ApplicationType applicationType) {
         final List<EvaluationItem> policeOfficerFeMaleData = getPoliceOfficeItemName(applicationType);
-
         evaluationItemRepository.saveAll(policeOfficerFeMaleData);
-
-        final List<List<Float>> policeOfficeFeMaleEvaluationData = new ArrayList<>();
-
-        policeOfficeFeMaleEvaluationData.add(List.of(0F, 25F, 28F, 31F, 34F, 36F, 38F, 40F, 42F, 44F));
-        policeOfficeFeMaleEvaluationData.add(List.of(0F, 7F, 10F, 13F, 16F, 19F, 22F, 25F, 28F, 31F));
-        policeOfficeFeMaleEvaluationData.add(List.of(0F, 23F, 27F, 31F, 35F, 39F, 43F, 47F, 51F, 55F));
-        policeOfficeFeMaleEvaluationData.add(List.of(0F, 10.15F, 9.91F, 9.67F, 9.43F, 9.19F, 8.95F, 8.71F, 8.47F, 8.23F));
-        policeOfficeFeMaleEvaluationData.add(List.of(0F, 24F, 28F, 32F, 35F, 38F, 41F, 44F, 47F, 51F));
-
+        final List<List<Float>> policeOfficeFeMaleEvaluationData = List.of(
+                List.of(0F, 25F, 28F, 31F, 34F, 36F, 38F, 40F, 42F, 44F),
+                List.of(0F, 7F, 10F, 13F, 16F, 19F, 22F, 25F, 28F, 31F),
+                List.of(0F, 23F, 27F, 31F, 35F, 39F, 43F, 47F, 51F, 55F),
+                List.of(0F, 10.15F, 9.91F, 9.67F, 9.43F, 9.19F, 8.95F, 8.71F, 8.47F, 8.23F),
+                List.of(0F, 24F, 28F, 32F, 35F, 38F, 41F, 44F, 47F, 51F)
+        );
         saveEvaluationSectionScore(policeOfficerFeMaleData, policeOfficeFeMaleEvaluationData);
         return policeOfficerFeMaleData;
     }
@@ -119,17 +113,14 @@ class DummyData {
     @Transactional
     public List<EvaluationItem> createFireOfficerMaleEvaluationItem(ApplicationType applicationType) {
         final List<EvaluationItem> fireOfficerMaleData = getFireOfficeItemName(applicationType);
-
         evaluationItemRepository.saveAll(fireOfficerMaleData);
-
-        final List<List<Float>> fireOfficerMaleSectionData = new ArrayList<>();
-
-        fireOfficerMaleSectionData.add(List.of(0F, 48.1F, 50.1F, 51.6F, 52.9F, 54.2F, 55.5F, 56.8F, 58.1F, 60F));
-        fireOfficerMaleSectionData.add(List.of(0F, 232F, 237F, 240F, 243F, 246F, 250F, 255F, 258F, 263F));
-        fireOfficerMaleSectionData.add(List.of(0F, 44F, 45F, 46F, 47F, 48F, 49F, 50F, 51F, 52F));
-        fireOfficerMaleSectionData.add(List.of(0F, 60F, 62F, 64F, 68F, 72F, 75F, 76F, 77F, 78F));
-        fireOfficerMaleSectionData.add(List.of(0F, 17.4F, 18.4F, 19.9F, 20.7F, 21.7F, 22.5F, 23.3F, 24.3F, 25.8F));
-
+        final List<List<Float>> fireOfficerMaleSectionData = List.of(
+                List.of(0F, 48.1F, 50.1F, 51.6F, 52.9F, 54.2F, 55.5F, 56.8F, 58.1F, 60F),
+                List.of(0F, 232F, 237F, 240F, 243F, 246F, 250F, 255F, 258F, 263F),
+                List.of(0F, 44F, 45F, 46F, 47F, 48F, 49F, 50F, 51F, 52F),
+                List.of(0F, 60F, 62F, 64F, 68F, 72F, 75F, 76F, 77F, 78F),
+                List.of(0F, 17.4F, 18.4F, 19.9F, 20.7F, 21.7F, 22.5F, 23.3F, 24.3F, 25.8F)
+        );
         saveEvaluationSectionScore(fireOfficerMaleData, fireOfficerMaleSectionData);
         return fireOfficerMaleData;
     }
@@ -137,17 +128,14 @@ class DummyData {
     @Transactional
     public List<EvaluationItem> createFireOfficerFeMaleEvaluationItem(ApplicationType applicationType) {
         final List<EvaluationItem> fireOfficerFeMaleData = getFireOfficeItemName(applicationType);
-
         evaluationItemRepository.saveAll(fireOfficerFeMaleData);
-
-        final List<List<Float>> fireOfficerFeMaleSectionData = new ArrayList<>();
-
-        fireOfficerFeMaleSectionData.add(List.of(0F, 29.0F, 30.3F, 31.2F, 32.0F, 33.0F, 33.8F, 34.7F, 35.8F, 37.0F));
-        fireOfficerFeMaleSectionData.add(List.of(0F, 165F, 169F, 173F, 177F, 181F, 185F, 189F, 194F, 199F));
-        fireOfficerFeMaleSectionData.add(List.of(0F, 34F, 35F, 36F, 37F, 38F, 39F, 40F, 41F, 42F));
-        fireOfficerFeMaleSectionData.add(List.of(0F, 29F, 31F, 32F, 34F, 37F, 40F, 41F, 42F, 43F));
-        fireOfficerFeMaleSectionData.add(List.of(0F, 20.7F, 21.7F, 22.7F, 23.5F, 24.9F, 25.5F, 26.2F, 26.8F, 28.0F));
-
+        final List<List<Float>> fireOfficerFeMaleSectionData = List.of(
+                List.of(0F, 29.0F, 30.3F, 31.2F, 32.0F, 33.0F, 33.8F, 34.7F, 35.8F, 37.0F),
+                List.of(0F, 165F, 169F, 173F, 177F, 181F, 185F, 189F, 194F, 199F),
+                List.of(0F, 34F, 35F, 36F, 37F, 38F, 39F, 40F, 41F, 42F),
+                List.of(0F, 29F, 31F, 32F, 34F, 37F, 40F, 41F, 42F, 43F),
+                List.of(0F, 20.7F, 21.7F, 22.7F, 23.5F, 24.9F, 25.5F, 26.2F, 26.8F, 28.0F)
+        );
         saveEvaluationSectionScore(fireOfficerFeMaleData, fireOfficerFeMaleSectionData);
         return fireOfficerFeMaleData;
     }
@@ -155,17 +143,14 @@ class DummyData {
     @Transactional
     public List<EvaluationItem> createCorrectionalOfficerMaleEvaluationItem(ApplicationType applicationType) {
         final List<EvaluationItem> correctionalOfficerMaleData = getCorrectionalOfficeMaleItemName(applicationType);
-
         evaluationItemRepository.saveAll(correctionalOfficerMaleData);
-
-        final List<List<Float>> correctionalOfficerMaleSectionData = new ArrayList<>();
-
-        correctionalOfficerMaleSectionData.add(List.of(0F, 130F, 140F, 150F, 160F, 170F, 180F, 190F, 200F, 210F));
-        correctionalOfficerMaleSectionData.add(List.of(0F, 230F, 235F, 240F, 245F, 250F, 255F, 260F, 265F, 270F));
-        correctionalOfficerMaleSectionData.add(List.of(0F, 35F, 38F, 41F, 44F, 47F, 50F, 53F, 56F, 59F));
-        correctionalOfficerMaleSectionData.add(List.of(0F, 10.20F, 10.05F, 9.90F, 9.75F, 9.60F, 9.45F, 9.30F, 9.15F, 9.00F));
-        correctionalOfficerMaleSectionData.add(List.of(0F, 10.00F, 9.40F, 9.20F, 9.00F, 8.40F, 8.20F, 8.00F, 7.40F, 7.20F));
-
+        final List<List<Float>> correctionalOfficerMaleSectionData = List.of(
+                List.of(0F, 130F, 140F, 150F, 160F, 170F, 180F, 190F, 200F, 210F),
+                List.of(0F, 230F, 235F, 240F, 245F, 250F, 255F, 260F, 265F, 270F),
+                List.of(0F, 35F, 38F, 41F, 44F, 47F, 50F, 53F, 56F, 59F),
+                List.of(0F, 10.20F, 10.05F, 9.90F, 9.75F, 9.60F, 9.45F, 9.30F, 9.15F, 9.00F),
+                List.of(0F, 10.00F, 9.40F, 9.20F, 9.00F, 8.40F, 8.20F, 8.00F, 7.40F, 7.20F)
+        );
         saveEvaluationSectionScore(correctionalOfficerMaleData, correctionalOfficerMaleSectionData);
         return correctionalOfficerMaleData;
     }
@@ -173,17 +158,14 @@ class DummyData {
     @Transactional
     public List<EvaluationItem> createCorrectionalOfficerFeMaleEvaluationItem(ApplicationType applicationType) {
         final List<EvaluationItem> correctionalOfficerFeMaleData = getCorrectionalOfficeFemaleItemName(applicationType);
-
         evaluationItemRepository.saveAll(correctionalOfficerFeMaleData);
-
-        final List<List<Float>> correctionalOfficerFeMaleSectionData = new ArrayList<>();
-
-        correctionalOfficerFeMaleSectionData.add(List.of(0F, 85F, 90F, 95F, 100F, 105F, 110F, 115F, 120F, 125F));
-        correctionalOfficerFeMaleSectionData.add(List.of(0F, 170F, 175F, 180F, 185F, 190F, 195F, 200F, 205F, 210F));
-        correctionalOfficerFeMaleSectionData.add(List.of(0F, 30F, 33F, 36F, 39F, 42F, 45F, 48F, 51F, 54F));
-        correctionalOfficerFeMaleSectionData.add(List.of(0F, 11.70F, 11.50F, 11.30F, 11.10F, 10.90F, 10.70F, 10.50F, 10.30F, 10.1F));
-        correctionalOfficerFeMaleSectionData.add(List.of(0F, 7.20F, 7.05F, 6.50F, 6.35F, 6.20F, 6.05F, 5.50F, 5.35F, 5.20F));
-
+        final List<List<Float>> correctionalOfficerFeMaleSectionData = List.of(
+                List.of(0F, 85F, 90F, 95F, 100F, 105F, 110F, 115F, 120F, 125F),
+                List.of(0F, 170F, 175F, 180F, 185F, 190F, 195F, 200F, 205F, 210F),
+                List.of(0F, 30F, 33F, 36F, 39F, 42F, 45F, 48F, 51F, 54F),
+                List.of(0F, 11.70F, 11.50F, 11.30F, 11.10F, 10.90F, 10.70F, 10.50F, 10.30F, 10.1F),
+                List.of(0F, 7.20F, 7.05F, 6.50F, 6.35F, 6.20F, 6.05F, 5.50F, 5.35F, 5.20F)
+        );
         saveEvaluationSectionScore(correctionalOfficerFeMaleData, correctionalOfficerFeMaleSectionData);
         return correctionalOfficerFeMaleData;
     }
