2cb065f2a31c91b2d7752998ad376a169c994c9e
ecsimsw <ecsimsw@gmail.com>
2023-08-17 19:17:34 +0900

    fix : sign-in

--- src/main/resources/static/js/sign-in.js
+++ src/main/resources/static/js/sign-in.js
@@ -24,11 +24,14 @@ function togglePasswordVisibility() {
       <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9ZM7.5 12C7.5 9.51472 9.51472 7.5 12 7.5C14.4853 7.5 16.5 9.51472 16.5 12C16.5 14.4853 14.4853 16.5 12 16.5C9.51472 16.5 7.5 14.4853 7.5 12Z" fill="#21272A"/>`;
     }
 }
-logInBtn.addEventListener("keyup", function (event) {
-      if (event.keyCode === 13) {
+
+passwordInput.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
         event.preventDefault();
         document.getElementById("logInBtn").click();
-      }
+    }
+})
+
 logInBtn.addEventListener("click", async function () {
     const email = emailInput.value;
     const pwd = passwordInput.value;
@@ -76,7 +79,7 @@ if (signUpText) {
     });
 }
 // sAlert('custom alert example!');
-function sAlert(txt, title = 'ERROR',) {
+function sAlert(txt, title = 'ERROR') {
     Swal.fire({
         title: title,
         text: txt,
