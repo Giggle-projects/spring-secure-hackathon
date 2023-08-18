cfeba2eaf1d224b2ab9069e5c54b4ae89a8995a8
ecsimsw <ecsimsw@gmail.com>
2023-08-18 10:10:06 +0900

    feat : admin blocking

--- src/main/java/se/ton/t210/utils/data/initData/InitDataMain.java
+++ src/main/java/se/ton/t210/utils/data/initData/InitDataMain.java
@@ -1,13 +1,13 @@
-package se.ton.t210.utils.data.initData;
-
-import java.io.IOException;
-
-public class InitDataMain {
-
-    public static void main(String[] args) throws IOException {
-        var csvLineStrategy = new MLTrainingCsvLine();
-        var initDataCSV = new InitDataCSV(csvLineStrategy, "ml_training_000_001.csv", 100_000);
-        initDataCSV.setInitialLine("id,member_id,applicationType,score,month");
-        initDataCSV.generate();
-    }
-}
+//package se.ton.t210.utils.data.initData;
+//
+//import java.io.IOException;
+//
+//public class InitDataMain {
+//
+//    public static void main(String[] args) throws IOException {
+//        var csvLineStrategy = new MLTrainingCsvLine();
+//        var initDataCSV = new InitDataCSV(csvLineStrategy, "ml_training_000_001.csv", 100_000);
+//        initDataCSV.setInitialLine("id,member_id,applicationType,score,month");
+//        initDataCSV.generate();
+//    }
+//}
